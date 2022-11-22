75671b2da079267981b05ef4a8a19f8969a1a4dc
MarcusVSSilva <marcussantana792@gmail.com>
2022-11-22 15:51:31 -0300

    instanciando objetos bootcamp e dev.

--- desafio-dio-poo/src/Main.java
+++ desafio-dio-poo/src/Main.java
@@ -2,7 +2,9 @@ import desafio_dominio.Mentoria;
 
 import java.time.LocalDate;
 
+import desafio_dominio.Bootcamp;
 import desafio_dominio.Curso;
+import desafio_dominio.Dev;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,9 +25,29 @@ public class Main {
         mentoria1.setDescricao("descrição mentoria java");
         mentoria1.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria1);
+        System.out.println(mentoria1);*/
+
+        //instanciando um bootcamp
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        //instanciando um dev
+        Dev devMarcus = new Dev();
+        devMarcus.setNome("Marcus");
+        devMarcus.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Marcus: " + devMarcus.getConteudosInscritos());
+
+        devMarcus.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Marcus: " + devMarcus.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Marcus: " + devMarcus.getConteudosConcluidos());
+        System.out.println("XP: " + devMarcus.calcularTotalXp());
 
     }
 }
