fff2e2f2e0188a774ea5037064f623d85dd6310b
MarcusVSSilva <marcussantana792@gmail.com>
2022-11-22 15:29:21 -0300

    metodos inscrever, progredir e calcularTotalXp.

--- desafio-dio-poo/src/desafio_dominio/Dev.java
+++ desafio-dio-poo/src/desafio_dominio/Dev.java
@@ -2,6 +2,7 @@ package desafio_dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -13,11 +14,26 @@ public class Dev {
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-    public void inscreverBootcamp (Bootcamp bootcamp){};
+    public void inscreverBootcamp (Bootcamp bootcamp){
+        //adicionar todos os conteudos do bootcamp
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        //adicionar o dev inscrito no bootcamp
+        bootcamp.getDevsInscritos().add(this);
+    };
 
-    public void progredir() {};
+    public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        }else{
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
+    };
 
-    public void calcularTotalXp() {};
+    public double calcularTotalXp() {
+        return this.conteudosConcluidos.stream().mapToDouble(conteudo -> conteudo.calcularXp()).sum();
+    };
 
     //métodos get e set (atributos com acesso privado)
     public String getNome(){
